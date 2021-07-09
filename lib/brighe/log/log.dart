66b972304214505c1fec895be5a854b1a40be3ee
v_lizhenyu <v_lizhenyu@tal.com>
2021-07-09 11:29:18 +0800

    调整启动样式

--- lib/brighe/log/log.dart
+++ lib/brighe/log/log.dart
@@ -3,23 +3,23 @@ import 'package:flutter/services.dart';
 const MethodChannel logChannel = MethodChannel('com.cvilia.flutter.log/log');
 
 class Log {
-  static void d(String msg, {String key = 'BubbleLog'}) {
+  static void d(String msg, String key) {
     logChannel.invokeMethod('d', {'key': key, 'message': msg});
   }
 
-  static void w(String msg, {String key = 'BubbleLog'}) {
+  static void w(String msg, String key) {
     logChannel.invokeMethod('w', {'key': key, 'message': msg});
   }
 
-  static void i(String msg, {String key = 'BubbleLog'}) {
+  static void i(String msg, String key) {
     logChannel.invokeMethod('i', {'key': key, 'message': msg});
   }
 
-  static void e(String msg, {String key = 'BubbleLog'}) {
+  static void e(String msg, String key) {
     logChannel.invokeMethod('e', {'key': key, 'message': msg});
   }
 
-  static void v(String msg, {String key = 'BubbleLog'}) {
+  static void v(String msg, String key) {
     logChannel.invokeMethod('v', {'key': key, 'message': msg});
   }
 }
